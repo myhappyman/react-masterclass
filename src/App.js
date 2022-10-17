e24e71f4ce8056b4b836e94b6c2a810cbd08f7b8
ParkShinwoo <fdsa@gdas.com>
2022-10-17 15:52:57 +0900

    styled components를 속성은 유지하고 태그명만 변경하는방법, 컴포넌트가 생성되면서 속성을 바로 입력하는 방법을 알아보았다.

--- src/App.js
+++ src/App.js
@@ -4,21 +4,40 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
+/**
+ * button태그의 style만 승계받고,
+ * 태그 자체인 button은 받고 싶지 않을때는 어떻게할까?
+ * 새롭게 정의하는게 아니라 컴포넌트를 생성하는 부분에서 as라는
+ * props를 사용하고 원하는 태그명을 입력하면 태그명이 변경되어 나온다.
+ */
+const Btn = styled.button`
+  color: #fff;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
 `;
 
-const Circle = styled(Box)`
-  border-radius: 50px;
+/**
+ * 또한 styled.태그명.다음으로attrs라는 속성을 줄 수 있는데,
+ * 이곳에 object형태로 넣고싶은 속성값들을 넣어주면 해당 속성이
+ * 추가된 상태로 컴포넌트가 생성된다.
+ */
+const Input = styled.input.attrs({ required: true })`
+  background-color: tomato;
 `;
 
 function App() {
   return (
-    <Father>
-      <Box bgColor="teal" />
-      <Circle bgColor="tomato" />
+    <Father as="header">
+      {/* <Btn>LogIn</Btn>
+      <Btn as="a" href="/">
+        새로고침
+      </Btn> */}
+      <Input maxLength="5" />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
     </Father>
   );
 }
