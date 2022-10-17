9b28152914e6caceb84ba2bd4433da409d775b55
ParkShinwoo <fdsa@gdas.com>
2022-10-17 15:39:54 +0900

    styled components를 통해 생성한 컴포넌트에 props를 통해 데이터를 전달하여 변경하고자 하는 속성만 변경하는법을 배우고, 컴포넌트를 확장하여 사용하는법을 학습하였다.

--- src/App.js
+++ src/App.js
@@ -4,27 +4,21 @@ const Father = styled.div`
   display: flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: teal;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
-const BoxTwo = styled.div`
-  background-color: tomato;
-  width: 100px;
-  height: 100px;
-`;
-const Text = styled.span`
-  color: #fff;
+
+const Circle = styled(Box)`
+  border-radius: 50px;
 `;
 
 function App() {
   return (
     <Father>
-      <BoxOne>
-        <Text>Hello</Text>
-      </BoxOne>
-      <BoxTwo />
+      <Box bgColor="teal" />
+      <Circle bgColor="tomato" />
     </Father>
   );
 }
