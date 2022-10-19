2ad42ad809a77bd221856b9d591b740445f2d7ac
ParkShinwoo <myhappyman@naver.com>
2022-10-19 12:02:41 +0900

    typescript json파일이 자동으로 생기지 않아서 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,9 @@
 import React from "react";
 import styled, { keyframes } from "styled-components";
 
-const rotateAnimation = keyframes`
-  0%{
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-  50%{
-    transform: rotate(360deg);
-    border-radius: 100px;
-  }
-  100%{
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-`;
-
-const Wrapper = styled.div`
-  display: flex;
-  height: 100vh;
-  width: 100vw;
-  justify-content: center;
-  align-items: center;
-  background-color: ${(props) => props.theme.bgColor};
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  animation: ${rotateAnimation} 1s linear infinite;
-  ${Emoji} {
-    &:hover {
-      font-size: 98px;
-    }
-  }
-  /*
-  위에서 span안에 &:hover는
-  span:hover{}와 동일하다.
-  */
-`;
-
-const Title = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
-
 function App() {
   return (
-    <Wrapper>
-      <Title>styled-component 학습 중</Title>
-      <Box>
-        <Emoji as="p">😊</Emoji>
-      </Box>
-      <Emoji>🔥</Emoji>
-    </Wrapper>
+    <div/>
   );
 }
 
