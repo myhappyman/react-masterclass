9c48274751ae94c66016a3cb9b1e13becf3b8767
ParkShinwoo <myhappyman@naver.com>
2022-10-25 12:25:00 +0900

    apexChart를 설치하고 react에서 차트를 넣는법을 살펴봤다. 또한 chart에 데이터를 넣는 부분에서 interface에 저장된 값에 따라 노출시켜야하는 값의 타입이 달라서 형변환 처리등을 하여 데이터의 구조를 맞춰주는 작업등을 하였다.

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,13 +1,66 @@
 import { useQuery } from "react-query";
 import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
+
+interface IHistroical{
+    time_open: number;
+    time_close: number;
+    open: string;
+    high: string;
+    low: string;
+    close: string;
+    volume: string;
+    market_cap: number;
+}
 
 interface ChartProps {
     coinId: string;
 }
 
 function Chart({coinId}:ChartProps){
-    const {isLoading, data} = useQuery(["ohlcv", coinId], ()=> fetchCoinHistory(coinId));
-    console.log(data);
-    return <h1>Chart</h1>
+    const {isLoading, data} = useQuery<IHistroical[]>(["ohlcv", coinId], ()=> fetchCoinHistory(coinId));
+    
+    return <div>
+        {isLoading ? "Loading..." : 
+            <ApexChart 
+                type="line"
+                series={[
+                    {
+                        name: "종가",
+                        data: data?.map((price => parseFloat(price.close))) ?? [],
+                    },
+                ]}
+                options={{
+                    theme: {
+                        mode:"dark"
+                    },
+                    chart: {
+                        height: 300,
+                        width: 500,
+                        toolbar: {
+                            show: false
+                        },
+                        background: "transparent"
+                    },
+                    grid: {
+                        show: false
+                    },
+                    stroke: {
+                        curve: "smooth",
+                        width: 4
+                    },
+                    yaxis: {show: false},
+                    xaxis: {
+                        labels: {show: false},
+                        axisTicks: {show: false},
+                        axisBorder: {show: false},
+                    }
+
+                }}
+
+                
+            />
+        }
+    </div>;
 }
 export default Chart;
